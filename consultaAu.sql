1f9258c077b6ce7e92820f8f435d87155301461b
RuRo <48282240+rurorita@users.noreply.github.com>
2020-09-15 17:22:59 +0200

    Update consultaAu.sql

--- consultaAu.sql
+++ consultaAu.sql
@@ -9,5 +9,12 @@ FROM alquileres
 INNER JOIN flota ON alquileres.matricula=flota.matricula 
 HAVING precio_final >12000;
 
+-- 3. Ver los datos de los alquileres de los coches matriculados en la provincia de Las Palmas
+SELECT alquileres.* 
+FROM alquileres 
+INNER JOIN flota ON flota.matricula=alquileres.matricula
+INNER JOIN distribuidores ON flota.codigo_distribuidor=distribuidores.codigo_distribuidor
+WHERE distribuidores.ciudad LIKE 'LAS%';
+
 -- 4. Realizar una consulta donde me indique las veces que ha sido alquilado cada coche
 SELECT matricula, COUNT(*) AS veces_alquilado FROM alquileres GROUP BY 1;
